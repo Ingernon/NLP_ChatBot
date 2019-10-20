4143cb4a41137ef57eca84ed854efe3f3a541638
Crawblue <kilian.le-grall@epitech.eu>
2019-10-21 00:03:14 +0800

    fix preprocessing ?

--- prep_data.py
+++ prep_data.py
@@ -5,10 +5,9 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.tag import pos_tag
 
-REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
-BAD_SYMBOLS_RE = re.compile('[^0-9a-z-A-Z #+_]')
-STOPWORDS = set(stopwords.words('english'))
-MAX_SIZE = 100
+REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;"]')
+BAD_SYMBOLS_RE = re.compile('[^0-9a-zA-Z -]')
+MAX_SIZE = 1000
 
 def preprocess(sent):
     sent = nltk.word_tokenize(sent)
@@ -18,19 +17,14 @@ def preprocess(sent):
 def preprocess_data(questions, answers):
 	i = 0
 	while i in range(len(questions)):
-		#if (str(answers[i]).find(".0") >= 0):
-		#	del answers[i]
-		#	del questions[i]
-		#else: 
 		str_answers = str(answers[i])
 		questions[i] = re.sub(REPLACE_BY_SPACE_RE, ' ', questions[i])
 		questions[i] = re.sub(BAD_SYMBOLS_RE, '', questions[i])
-		questions[i] = ' '.join(t for t in questions[i].split() if t not in STOPWORDS)
-		answers[i] = re.sub(REPLACE_BY_SPACE_RE, ' ', str_answers)
+		answers[i] = re.sub(REPLACE_BY_SPACE_RE, '', str_answers)
 		answers[i] = re.sub(BAD_SYMBOLS_RE, '', str_answers)
-		answers[i] = ' '.join(t for t in str_answers.split() if t not in STOPWORDS)
 		questions[i] = questions[i].lower()
 		answers[i] = str_answers.lower()
+		# print(questions[i], "\n", answers[i], "\n")
 		i +=1
 	return questions, answers
 
